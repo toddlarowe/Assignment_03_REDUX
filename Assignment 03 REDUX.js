eaba27ef5dfaee0ab90aea55e6a0d50f1fd4144f
toddlarowe <32879848+toddlarowe@users.noreply.github.com>
2017-10-22 13:49:17 -0700

    Assignment 03 Resubmission

---
+++ Assignment 03 REDUX.js
@@ -0,0 +1,29 @@
+//Assignment 03 REDUX
+//Ok, I know there are many issues but I've been struggling with this for too long and need to move on, now, sorry. And thanks.
+
+function do_something(in_string, in_number) {
+
+//  let a, b, c, x;
+
+  let a = 1;
+  let b = a + 1;
+
+  for(i = 0; i = (in_number == 1); i++) {
+    let x = in_string.charCodeAt(i);
+    if(in_string[x] == 32 || in_string[x] <=65 || in_string[x] >= 122) {
+       in_number = in_number - 1;
+    } a = a + 1;
+      b = b + 1;
+  }
+
+  for(i = 0; i = in_string.length; i++) {
+    if(a == b) {
+      let c = c + in_string[a];
+    } a = a + 1
+  }
+      return c;
+}
+
+console.log(do_something("the quick brown fox", 3));
+
+And this is how we roll...
